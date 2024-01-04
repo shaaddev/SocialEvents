d7f6dd873130a50783ab51cfd4606b6a7cee5eb5
Rashaad Lee Hue <rashaadleehue1@gmail.com>
2024-01-04 02:32:10 -0400

    revert

--- social/settings.py
+++ social/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ['secret_key']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '0.0.0.0']
+ALLOWED_HOSTS = []
 
 
 # Application definition
